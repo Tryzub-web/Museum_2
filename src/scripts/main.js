62193024897a52c3d9c3207ec1b44890b7ac13fa
Tryzub <prudkyi@yahoo.com>
2026-07-02 18:25:07 +0200

    Ok

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const language = document.querySelector('.top-bar__language');
+const languageList = document.querySelector('.top-bar__language-list');
+
+language.addEventListener('click', () => {
+  languageList.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('.top-bar__language')) {
+    languageList.classList.remove('active');
+  }
+});
